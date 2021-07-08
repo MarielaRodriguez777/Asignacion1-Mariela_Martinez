11839fc5d218789bca36188e8d7d07dfee08c6fa
Mariela Martinez <mariela_rodriguez52@yahoo.es>
2021-07-07 18:19:46 -0600

    maquetacion

---
+++ asignacion1/src/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Styles.css'
+
+export const Landing = () => {
+    return (
+        <>
+        <header>
+            <nav>
+                <ul>
+                    <li><img src=""></img></li>
+                    <li><a>About Us</a></li>
+                    <li><a>About Us</a></li>
+                    <li>
+                        <input type="search"></input>
+                    </li>
+                    <li><a>About Us</a></li>
+                    <li><a>About Us</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div>
+            <h1>Hola mundo</h1>
+        </div>
+        </>
+    )
+}
