d06c46db2a7e9ab2eecec1c785feaa0cd4ebea6d
Mariela Martinez <mariela_rodriguez52@yahoo.es>
2021-07-27 14:13:57 -0600

    tarjetas en la pantalla de producto

--- asignacion1/src/components/ProductPage/ViewProducts.js
+++ asignacion1/src/components/ProductPage/ViewProducts.js
@@ -1,26 +1,50 @@
 import React from "react";
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
+import StarRating from "../starRating/StarRating";
 
-export const ViewProducts = ({article}) => {
+export const ViewProducts = ({ article }) => {
   const history = useHistory();
 
-  const handleSeeMore = () =>{
+  const handleSeeMore = () => {
     history.push(`/screens/especificProduct/${article.id}`);
-  }
+  };
   return (
-    <div className="tarjet">
-      <div className="generalInfo">
-        <img
-          className="product-image"
-          src={article.image}
-          alt=""
-        />
-        <h4>{article.productName}</h4>
-        <span>{article.descripcionCorta}</span>
+    <>
+      {/* <div className="tarjet">
+        <div className="generalInfo">
+          <img className="product-image" src={article.image} alt="" />
+          <h4>{article.productName}</h4>
+          <span>{article.descripcionCorta}</span>
+        </div>
+        <div className="seeMore">
+          <span onClick={handleSeeMore}>See more</span>
+        </div>
+      </div> */}
+
+      <div className="cardProduct">
+        <div className="card-image">
+          <img
+            className="card-image"
+            src={`${article.image}`}
+            alt=""
+          />
+        </div>
+        <div className="card-textProduct">
+          <span className="date">{article.creationDate}</span>
+          <h2>{article.productName}</h2>
+          <div className="descripcion">
+            <span>{article.descripcionCorta}</span>
+          </div>
+        </div>
+        <div className="nose">
+          <div className="starProduct">
+            <StarRating />
+          </div>
+          <div className="seeMoreButton">
+            <span onClick={handleSeeMore}>See more</span>
+          </div>
+        </div>
       </div>
-      <div className="seeMore">
-        <span onClick={handleSeeMore}>See more</span>
-      </div>
-    </div>
+    </>
   );
 };
